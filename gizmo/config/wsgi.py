18405918f2471d23943e10f8f1e9eec74f7ee4ca
Don Button <donpublic@digitalgizmo.com>
2021-08-02 12:25:54 -0400

    change wsgi.py to production

--- gizmo/config/wsgi.py
+++ gizmo/config/wsgi.py
@@ -7,22 +7,32 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# # from django.core.wsgi import get_wsgi_application
+# # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
+# # application = get_wsgi_application()
+
+# # from Two Scoops
+# try:
+# 	import mod_wsgi
+# 	try:
+# 		os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.%s' % mod_wsgi.process_group
+# 	except AttributeError:
+# 		# let the above setting stand
+# 		os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+# except ImportError:
+# 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+	
 # from django.core.wsgi import get_wsgi_application
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
 # application = get_wsgi_application()
 
-# from Two Scoops
-try:
-	import mod_wsgi
-	try:
-		os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.%s' % mod_wsgi.process_group
-	except AttributeError:
-		# let the above setting stand
-		os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-except ImportError:
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-	
+# As of 2021, Django 3.2, using gunicorn instead of mod_wsgi
+import os
+
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+
 application = get_wsgi_application()
+
